91590e6dca53ebdb29ae16d04f57b5ee16663d4b
Caio Dias <kiobreno@gmail.com>
2017-02-04 17:23:00 -0500

    Add basic memory db

---
+++ CleanerSeeker/CleanerSeeker/MemoryDatabase.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryDatabase.swift
+//  CleanerSeeker
+//
+//  Created by Caio Dias on 2017-02-04.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+class MemoryDatabase {
+    private let _users: [User]
+    private let _jobs: [Job]
+    
+    init() {
+        self._users = []
+        self._jobs = []
+    }
+}
