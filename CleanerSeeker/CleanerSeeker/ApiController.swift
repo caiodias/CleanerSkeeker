b2828ca51b3a08a218aba14a1f81180f1d486b4a
Caio Dias <kiobreno@gmail.com>
2017-02-04 16:40:48 -0500

    Add API Controller

---
+++ CleanerSeeker/CleanerSeeker/ApiController.swift
@@ -0,0 +1,16 @@
+//
+//  ApiController.swift
+//  CleanerSeeker
+//
+//  Created by Caio Dias on 2017-02-04.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import Foundation
+
+class ApiController {
+    
+    func registerUser() -> String {
+        return "Caio"
+    }
+}
