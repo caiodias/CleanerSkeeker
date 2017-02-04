d7103c8986ae731f3288afb4b76b9ec2c9cd19a9
Caio Dias <kiobreno@gmail.com>
2017-02-04 16:41:03 -0500

    Add the basic Utilities class

---
+++ CleanerSeeker/CleanerSeeker/Utilities.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities.swift
+//  CleanerSeeker
+//
+//  Created by Caio Dias on 2017-02-04.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import Foundation
+
+class Utilities {
+    static let programName = "Job Seeker"
+    
+    static func getProgamName() -> String {
+        return "Job Seeker"
+    }
+}
