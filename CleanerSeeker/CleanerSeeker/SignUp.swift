26dcf185444e8805884975885637fdcc1cea2886
Arpita Patel <arpita.8392.patel@gmail.com>
2017-03-21 18:04:05 -0400

    add signup class

---
+++ CleanerSeeker/CleanerSeeker/SignUp.swift
@@ -0,0 +1,13 @@
+//
+//  SignUp.swift
+//  CleanerSeeker
+//
+//  Created by Arpita Patel on 2017-03-21.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import Foundation
+
+class SignUp {
+    
+}
