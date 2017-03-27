68f414144ab880238e0a2d85108c2b06173db834
Caio Dias <kiobreno@gmail.com>
2017-03-27 16:44:13 -0400

    - Rename the ViewController (not being used) to BasicVC - Add FindJobOffersVC class and use the Basic VC class

---
+++ CleanerSeeker/CleanerSeeker/FindJobOffersVC.swift
@@ -0,0 +1,17 @@
+//
+//  FindJobOffersVC.swift
+//  CleanerSeeker
+//
+//  Created by Caio Dias on 2017-03-27.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import UIKit
+
+class FindJobOffersVC: BasicVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
