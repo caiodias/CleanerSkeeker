585e62adda99e9298c6c817a936f346dd88c222d
Arpita Patel <arpita.8392.patel@gmail.com>
2017-03-27 17:15:40 -0400

    Post List and Add new Post Screen

---
+++ CleanerSeeker/CleanerSeeker/AddNewPostViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddNewPostViewController.swift
+//  CleanerSeeker
+//
+//  Created by Arpita Patel on 2017-03-27.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import UIKit
+
+class AddNewPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
