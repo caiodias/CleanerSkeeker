ed048417d0a32657b81ec8e0fe624ddc321d5901
Arpita Patel <arpita.8392.patel@gmail.com>
2017-04-03 15:35:45 -0400

    remove comment code… no need it more

--- CleanerSeeker/CleanerSeeker/AddNewPostViewController.swift
+++ CleanerSeeker/CleanerSeeker/AddNewPostViewController.swift
@@ -46,10 +46,6 @@ class AddNewPostViewController: UIViewController, UIPickerViewDelegate, UIPicker
 
         noOfWashrooms = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                          "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
-
-//        noOfBedrooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-//                        11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
     }
 
     override func didReceiveMemoryWarning() {
