e387965c3f97ad8611b04fb16cb1eb0248027183
Caio Dias <kiobreno@gmail.com>
2017-04-08 13:39:51 -0400

    Add list jobs

--- CleanerSeeker/CleanerSeeker/JobPoster.swift
+++ CleanerSeeker/CleanerSeeker/JobPoster.swift
@@ -9,6 +9,10 @@
 import Parse
 
 class JobPoster: PFObject, PFSubclassing {
+    enum JobPosterErrors: Error {
+        case JobNotFound
+    }
+
     @NSManaged var firstName: String
     @NSManaged var lastName: String
     @NSManaged var latitude: Double
@@ -16,36 +20,11 @@ class JobPoster: PFObject, PFSubclassing {
     @NSManaged var address: String
     @NSManaged var jobOpportunities: [JobOpportunity]
 
-    /*
-     GIVES AN ERROR  Use only PFObject constructor
-     convenience override init() {
-        self.init(firstName: "", lastName: "", address: "", latitude: 0.0, longitude: 0.0, avatar: "")
-    }
-
-    convenience init(firstName: String, lastName: String) {
-        self.init(firstName: firstName, lastName: lastName, address: "", latitude: 0.0, longitude: 0.0, avatar: "")
-    }
-
-    init(firstName: String, lastName: String, address: String, latitude: Double, longitude: Double, avatar: String) {
-        super.init()
-
-        self.firstName = firstName
-        self.lastName = lastName
-        self.address = address
-        self.latitude = latitude
-        self.longitude = longitude
-        self.avatar = avatar
-
-    }
-
-    // MARK: User Protocol methods
-    func clone() -> JobPoster {
-        return JobPoster(firstName: self.firstName, lastName: self.lastName, address: self.address, latitude: self.latitude, longitude: self.longitude, avatar: self.avatar)
-    }*/
-
-    // MARK: PFSubclassing Protocol methods
-
     class func parseClassName() -> String {
         return "JobPoster"
+    }
+
+    func getJobs(status: JobStatus) throws -> [JobOpportunity] {
+        return self.jobOpportunities.filter({ $0.status == status.rawValue })
     }
 }
