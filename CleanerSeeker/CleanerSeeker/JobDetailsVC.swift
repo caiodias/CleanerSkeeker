b913c80f9f84ed114a412df6a7205fa92453f2af
Caio Dias <kiobreno@gmail.com>
2017-04-22 12:50:09 -0400

    Add JobDetails Screen and ViewController

---
+++ CleanerSeeker/CleanerSeeker/JobDetailsVC.swift
@@ -0,0 +1,13 @@
+//
+//  JobDetailsVC.swift
+//  CleanerSeeker
+//
+//  Created by Caio Dias on 2017-04-22.
+//  Copyright © 2017 Caio Dias. All rights reserved.
+//
+
+import Foundation
+
+class JobDetailsVC: BasicVC {
+    
+}
